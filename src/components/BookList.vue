a9155767a201701f8b605e2cc4e5591fecf1f1b7
YuantaoYin <yuantao.yin@delta.com>
2019-08-31 02:50:34 -0400

    update

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -9,7 +12,13 @@
         name: 'BookList',
         data() {
             return {
-                title: 'All Books'
+                title: 'All Books',
+                books: [
+                    {title: 'Self-Reliance', author: 'RalphWaldo Emerson'},
+                    {title: 'American news', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves', author: 'Postman'}
+
+                ]
             }
         }
     }
@@ -18,5 +27,13 @@
 <style>
     h1, h2 {
         font-weight: normal
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
